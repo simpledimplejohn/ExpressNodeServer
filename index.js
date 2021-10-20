c809acee3198bc0f5461b323d2b490298b215616
Simple Dimple John <simpledimplejohn@gmail.com>
2021-10-20 11:02:45 -0700

    add fetch code, server now recieves data, works

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ const app = express();
 app.listen(3000, () => console.log('listening at 3000'));
 //tells the server to go to the public folder-default is to index.html
 app.use(express.static('public'));
+//to handle JSON and protects server from a data dump
+app.use(express.json({limit: '1mb'}))
 //address that we will recieve the post, next is the call back function 
 app.post('/api', (request, response) => {
-  console.log(request);
+  console.log("I got a request!");
+  console.log(request.body);
 })

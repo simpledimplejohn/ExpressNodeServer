440308ed4a19006c8389ebf4588a89bc4341f5c4
Simple Dimple John <simpledimplejohn@gmail.com>
2021-10-20 09:36:44 -0700

    add index.js calling express--works

---
+++ index.js
@@ -0,0 +1,7 @@
+//how to launch express--save express into a function, call that function 
+
+
+const express = require('express');
+const app = express();
+// call the app--listen at port 3000, then call back function
+app.listen(3000, () => console.log('listening at 300'));

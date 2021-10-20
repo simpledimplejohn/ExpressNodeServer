9f0edf73ca127038171c237b0bc2cdd7624f511f
Simple Dimple John <simpledimplejohn@gmail.com>
2021-10-20 09:41:44 -0700

    add index.html and connected

--- index.js
+++ index.js
@@ -4,4 +4,6 @@
 const express = require('express');
 const app = express();
 // call the app--listen at port 3000, then call back function
-app.listen(3000, () => console.log('listening at 300'));
+app.listen(3000, () => console.log('listening at 3000'));
+//
+app.use(express.static('public'));

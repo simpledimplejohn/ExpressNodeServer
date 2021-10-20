14db8cad24dab9c732afb17343825476c998475a
Simple Dimple John <simpledimplejohn@gmail.com>
2021-10-20 10:23:31 -0700

    add lat and long taken from navigator object

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 //how to launch express--save express into a function, call that function 
-
+//all server function is run in here
 
 const express = require('express');
 const app = express();
 // call the app--listen at port 3000, then call back function
 app.listen(3000, () => console.log('listening at 3000'));
-//
+//tells the server to go to the public folder-default is to index.html
 app.use(express.static('public'));

c878b384fa084343955d5b5c37471bfc2d04a9f6
Simple Dimple John <simpledimplejohn@gmail.com>
2021-10-21 10:05:09 -0700

    add database call with placeholder object

--- index.js
+++ index.js
@@ -2,16 +2,22 @@
 //all server function is run in here
 
 const express = require('express');
+const Datastore = require('nedb');
+
 const app = express();
 // call the app--listen at port 3000, then call back function
 app.listen(3000, () => console.log('listening at 3000'));
 //tells the server to go to the public folder-default is to index.html
 app.use(express.static('public'));
 //to handle JSON and protects server from a data dump
-app.use(express.json({limit: '1mb'}))
+app.use(express.json({limit: '1mb'}));
 
-// make an array to store the data from the API call
-const database = [];
+// an object that's stored in the nedb format
+const database = new Datastore('database.db');
+//actually loads the file
+database.loadDatabase();
+//hard code into the database:
+database.insert({name: "potato", color: "brown", eyes: 3, cooked: false});
 
 //address that we will recieve the post, next is the call back function 
 app.post('/api', (request, response) => {
@@ -20,7 +26,7 @@ app.post('/api', (request, response) => {
   //you always need a response after your request send afterwards
   //use .then to handle the promise that fetch returns
   const data = request.body;
-  database.push(data);
+  database.push(data); 
   console.log("the data is :", data);
   console.log("the database is", database);
 
